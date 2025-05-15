c25ed1c1f12a583059747018aff54857ea6a0178
ShellyCS <mailstosaily@gmail.com>
2025-05-15 00:17:58 -0400

    Chart

---
+++ src/components/Featured/Featured.jsx
@@ -0,0 +1,48 @@
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import "./Featured.scss";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <h3>Total Revenue</h3>
+      <div className="content">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$420</p>
+        <span className="description">
+          Previous transactions processing. Last payments may not be included.
+        </span>
+        <div className="detail-cont">
+          <div className="detail">
+            <div className="title">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="detail">
+            <div className="title">Last Week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="detail">
+            <div className="title">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import "./home.scss";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Navbar from "../../components/Navbar/Navbar";
 import Widget from "../../components/Widget/Widget";
+import Featured from "../../components/Featured/Featured";
+import Chart from "../../components/Chart/Chart";
 
 const Home = () => {
   return (
@@ -10,12 +12,17 @@ const Home = () => {
       <Sidebar />
       <div className="nav-cont">
         <Navbar />
+        <hr style={{ height: "0", border: "0.5px solid rgb(230, 227, 227)" }} />
         <div className="home-container">
           <div className="widgets">
             <Widget type="user" />
             <Widget type="order" />
             <Widget type="earning" />
             <Widget type="balance" />
+          </div>
+          <div className="charts">
+            <Featured />
+            <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
           </div>
         </div>
       </div>
