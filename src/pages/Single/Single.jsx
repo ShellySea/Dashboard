c24a1f185934f32a86ff954f79bc3629e1d6d868
ShellyCS <mailstosaily@gmail.com>
2025-05-17 20:31:23 -0400

    Single page, on view button.

--- src/pages/Single/Single.jsx
+++ src/pages/Single/Single.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import "./Single.scss";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/Tables/Tables";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="single-container">
+        <Navbar />
+        <div className="top-container">
+          <div className="profile-card">
+            <div className="top">
+              <h1 className="title">Information</h1>
+              <button className="edit-button">Edit</button>
+            </div>
+            <div className="bottom">
+              <div className="left">
+                <img
+                  src="https://www.perfocal.com/blog/content/images/size/w1920/2021/01/Perfocal_17-11-2019_TYWFAQ_100_standard-3.jpg"
+                  alt="profile"
+                  className="profile-pic"
+                />
+              </div>
+              <div className="right">
+                <div className="details">
+                  <h1 className="itemTitle">Jane Doe</h1>
+                  <div className="detailItem">
+                    <span className="itemKey">Email:</span>
+                    <span className="itemValue">janedoe@gmail.com</span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Phone:</span>
+                    <span className="itemValue">+1 2345 67 89</span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Address:</span>
+                    <span className="itemValue">
+                      Elton St. 234 Garden Yd. NewYork
+                    </span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Country:</span>
+                    <span className="itemValue">USA</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="graph">
+            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
+          </div>
+        </div>
+        <div className="bottom-container">
+          <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
